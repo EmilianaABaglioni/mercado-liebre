b52551d7d19e6028ca5736ec0717c6578fe46c1f
EmilianaABaglioni <emi.baglioni@outlook.com>
2022-08-02 22:25:14 -0300

    Mercado-liebre / Digital House

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3030;
+
+const publicPath = path.join(__dirname, "/public");
+app.use(express.static(publicPath));
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+});
